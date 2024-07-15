7e2e18b423757aac39f68aa3855f89516364847f
Matheus Fillipe <matheusfillipe@hotmail.com>
2024-07-15 11:34:45 -0300

    assets e configurando estilos

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      'project-red': '#E3646E',
+      'project-purple': '#BB72E9',
+      'project-blue': '#3996DB',
+      'project-green': '#82BC4F',
+      'project-yellow': '#EABD5F',
+      'gray-100': '#0D0E11',
+      'gray-200': '#16181D',
+      'gray-300': '#292C34',
+      'gray-400': '#878EA1',
+      'gray-500': '#C0C4CE',
+      'gray-600': '#E2E4E9',
+    },
+  },
+  extend: {
   },
   plugins: [],
 }
